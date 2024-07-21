ac88aa77cfb4df8f266bbc4d5576668e0b090b8c
xgrenc01 <dominikgrencik@gmail.com>
2024-07-21 18:02:22 +0200

    some table style edits

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -95,8 +95,7 @@ const Table = <T extends Record<string, string | number | boolean>>({
                               )}
                             </div>
                           ) : (
-                            <div className="filter">
-                              <input disabled type="text" />
+                            <div className="no-filter">
                             </div>
                           )}
                         </div>
